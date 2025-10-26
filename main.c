9ffdc54c9a83258e8a9be50344238cb99c0ff168
楽しいウルフガン <funwolfgun@gmail.com>
2025-10-26 21:17:59 +0100

    Esempio di struct prodotto

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+struct prodotto
+{
+    char nome[50];
+    int id;
+    double prezzo;
+    int quantita;
+};
 int main(void) {
-    printf("Hello, World!\n");
+    struct prodotto prodotto;
+    strcpy(prodotto.nome, "Prodotto");
+    prodotto.id = 1;
+    prodotto.prezzo = 1.78f;
+    prodotto.quantita = 1;
+
+    printf("--- Scheda Prodotto ---\n");
+    printf("Nome: %s\n", prodotto.nome);
+    printf("Codice: %d\n", prodotto.id);
+    printf("Prezzo: %.2f euro\n", prodotto.prezzo);
+    printf("Quantita': %d unita'\n", prodotto.quantita);
     return 0;
 }
